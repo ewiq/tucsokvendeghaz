8d6f4f87f9618fa875f69f8b92e4f250765dc5b4
thorpiches <pelletkristof@gmail.com>
2025-12-03 13:21:09 +0100

    Updates

---
+++ _app/immutable/chunks/CPSsQpl0.js
@@ -0,0 +1,1 @@
+const s=globalThis.__sveltekit_1qlwpby?.base??"/tucsokvendeghaz",e=globalThis.__sveltekit_1qlwpby?.assets??s??"";export{e as a,s as b};
--- _app/immutable/chunks/DCB_XEAQ.js
+++
@@ -1,1 +0,0 @@
-const s=globalThis.__sveltekit_1kdvuxm?.base??"/tucsokvendeghaz",e=globalThis.__sveltekit_1kdvuxm?.assets??s??"";export{e as a,s as b};
